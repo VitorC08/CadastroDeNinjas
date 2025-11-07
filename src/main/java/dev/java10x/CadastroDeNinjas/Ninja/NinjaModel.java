339b74684d304765e08251ea0d00ee925531b811
Vitor <costasousavitor@outlook.com>
2025-11-06 23:38:32 -0300

    feat: adicionando lombok nas classes, para abstração

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninja/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninja/NinjaModel.java
@@ -2,11 +2,16 @@ package dev.java10x.CadastroDeNinjas.Ninja;
 
 import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.List;
 
 @Entity
 @Table(name = "tb_cadastro")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class NinjaModel {
 
      @Id
@@ -20,37 +25,4 @@ public class NinjaModel {
      @JoinColumn(name = "missoes_id")
      private MissoesModel missoes;
 
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String name, String email, int idade){
-        this.name = name;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
