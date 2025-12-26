c803df7540da1cf52937a2dee1ba60bb024e7a16
Vitor <costasousavitor@outlook.com>
2025-12-26 03:09:50 -0300

    chore: adicionando DTO e Mapper para as missoes

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesDTO.java
@@ -0,0 +1,48 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+import dev.java10x.CadastroDeNinjas.Ninja.NinjaModel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class MissoesDTO {
+    private Long id;
+    private String nome;
+    private String dificuldade;
+    private List<NinjaModel> ninjas;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDificuldade() {
+        return dificuldade;
+    }
+
+    public void setDificuldade(String dificuldade) {
+        this.dificuldade = dificuldade;
+    }
+
+    public List<NinjaModel> getNinjas() {
+        return ninjas;
+    }
+
+    public void setNinjas(List<NinjaModel> ninjas) {
+        this.ninjas = ninjas;
+    }
+}
